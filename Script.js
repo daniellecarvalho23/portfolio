d2a12122c7fd19946938ba7328263114c8b17d54
daniellecarvalho23 <daniellefraga23@gmail.com>
2024-10-31 19:45:01 -0700

    Fixing Download

--- Script.js
+++ Script.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     downloadButton.addEventListener("click", function() {
         const link = document.createElement("a");
-        link.href = "Images/DanielleCarvalho_Resume.pdf"; // Adjust to the correct path and file extension
+        link.href = "https://cdn.discordapp.com/attachments/799976470893953034/1301726109846732843/DanielleCarvalho_Resume.pdf?ex=67258669&is=672434e9&hm=1597639a1e8c49fc3e2344e0b3abb8f0c1474852fc3ae06fd32de2cba8ae9d57&"; // Adjust to the correct path and file extension
         link.target = "_blank"; // Opens in a new tab
         document.body.appendChild(link);
         link.click();
